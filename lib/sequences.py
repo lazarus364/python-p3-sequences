df7bac37786d3c450a1d9e38dea981aacc5bf6bb
lazarus kipchumba <lazaruskipchumba6@gmail.com>
2025-01-12 14:39:31 +0300

    python

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])  
+        return
+    elif length == 1:
+        print([0])  
+        return
+
+    fib = [0, 1]
+    for _ in range(2, length):
+        fib.append(fib[-1] + fib[-2])
+    
+    print(fib)  
+print_fibonacci(0)  
+print_fibonacci(1)  
+print_fibonacci(2)  
+print_fibonacci(10)
